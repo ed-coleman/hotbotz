54383f01e940038c80ec298600e81f6c6d734326
Krystina Witt <krystinawitt@gmail.com>
2023-02-13 19:14:05 +0100

    add scoville and date formatted on sauces filters on home page

--- seeds/sauces.seed.js
+++ seeds/sauces.seed.js
@@ -21,7 +21,7 @@ const saucesArr = [{
     "originCountry": "united states of america",
     "peppers": "Chiles (green serrano, applewood smoked  green serrano, orange habanero)",
     "scoville": "5000",
-    "updatedAt": "2023-02-11T14:07:36.068Z"
+    "updatedAt": "2023-01-11T14:07:36.068Z"
   },{
     "addedBy": "63e63f130c6136cbc2ff5e88",
     "createdAt": "2007-05-25T06:51:27.000+00:00",
@@ -34,7 +34,7 @@ const saucesArr = [{
     "originCountry": "united states of america",
     "peppers": "Organic chile de arbol peppers",
     "scoville": "3000",
-    "updatedAt": "2023-02-11T14:18:55.499Z"
+    "updatedAt": "2023-01-11T14:18:55.499Z"
   },{
     "addedBy": "63e63f130c6136cbc2ff5e88",
     "createdAt": "2007-05-25T06:51:27.000+00:00",
@@ -47,7 +47,7 @@ const saucesArr = [{
     "originCountry": "united states of america",
     "peppers": "Chili peppers",
     "scoville": "1000",
-    "updatedAt": "2023-02-11T14:19:54.193Z"
+    "updatedAt": "2023-01-11T14:19:54.193Z"
   },{
     "addedBy": "63e63f130c6136cbc2ff5e88",
     "createdAt": "2007-05-25T06:51:27.000+00:00",
@@ -60,7 +60,7 @@ const saucesArr = [{
     "originCountry": "united states of america",
     "peppers": "Fresno chile pepper",
     "scoville": "1000",
-    "updatedAt": "2023-02-11T14:07:36.068Z"
+    "updatedAt": "2023-01-11T14:07:36.068Z"
   },{
     "addedBy": "63e63f130c6136cbc2ff5e88",
     "createdAt": "2007-05-25T06:51:27.000+00:00",
@@ -73,7 +73,7 @@ const saucesArr = [{
     "originCountry": "united states of america",
     "peppers": "Habanero Chili",
     "scoville": "135600",
-    "updatedAt": "2023-02-11T14:18:55.499Z"
+    "updatedAt": "2023-01-11T14:18:55.499Z"
   },{
     "addedBy": "63e63f130c6136cbc2ff5e88",
     "createdAt": "2007-05-25T06:51:27.000+00:00",
@@ -86,7 +86,7 @@ const saucesArr = [{
     "originCountry": "united states of america",
     "peppers": "habanero peppers, Cayenne peppers",
     "scoville": "1000",
-    "updatedAt": "2023-02-11T14:19:54.193Z"
+    "updatedAt": "2023-01-11T14:19:54.193Z"
   },{
     "addedBy": "63e63f130c6136cbc2ff5e88",
     "createdAt": "2007-05-25T06:51:27.000+00:00",
@@ -99,7 +99,7 @@ const saucesArr = [{
     "originCountry": "united states of america",
     "peppers": "green chile peppers",
     "scoville": "1000",
-    "updatedAt": "2023-02-11T14:07:36.068Z"
+    "updatedAt": "2023-01-11T14:07:36.068Z"
   },{
     "addedBy": "63e63f130c6136cbc2ff5e88",
     "createdAt": "2023-01-25T06:51:27.000+00:00",
@@ -141,7 +141,7 @@ const saucesArr = [{
     "updatedAt": "2023-01-11T14:07:36.068Z"
   },{
     "addedBy": "63e63f130c6136cbc2ff5e88",
-    "createdAt": "2023-03-25T06:51:27.000+00:00",
+    "createdAt": "2023-01-11T06:51:27.000+00:00",
     "description": "From the folks that brought you Season 6’s Heartbeat Habanero, Pineapple Habanero opens on the palate with sweet fruit before upping the ante with sharper, peppery notes. The sauce finishes with subtle hoppiness from the Sleeping Giant Brewing Company’s session IPA and lingering habanero heat. Try it with tacos al pastor, on a grilled chicken sandwich, or in an end-of-summer margarita.",
     "image": "https://cdn.shopify.com/s/files/1/2086/9287/products/pineapple.jpg?v=1628204289&width=680",
     "ingredients": "Pineapple, yellow peppers, onion, distilled vinegar, habaneros, sugar, lime juice, kosher salt, garlic, canola oil, ale (water, hops, malted barley, yeast)",
@@ -154,7 +154,7 @@ const saucesArr = [{
     "updatedAt": "2025-02-11T14:18:55.499Z"
   },{
     "addedBy": "63e63f130c6136cbc2ff5e88",
-    "createdAt": "2023-03-25T03:51:27.000+00:00",
+    "createdAt": "2023-01-11T03:51:27.000+00:00",
     "description": "Featured on Hot Ones Season 10! Blazing hot chilis meet savory Aka Miso to create the perfect umami bomb. Buckle up, it's a wild ride.",
     "image": "https://uploads-ssl.webflow.com/623a2b510ddc084808eb61a5/63522f9b52cdcb7b5f981a77_amgr-silo-mid-min.png",
     "ingredients": "Red Ghost Pepper Puree (Red Ghost Chili Peppers, Salt, Acetic Acid), Rice Vinegar, Carolina Reaper Pepper Puree (Carolina Reaper Peppers, Salt, Acetic Acid), Miso Paste (Organic Soybeans, Organic Rice Koji (Organic Rice, Koji Spores, [Aspergillus Oryzae]), Sea Salt, Water), Tamari Sauce (Water, Soybeans, Salt, Alcohol (To Preserve Freshness), Pure Sesame Oil, Japanese Seven Spice (Spices, Sesame Seeds, Orange Peel, Salt, Nori (Seaweed)",
@@ -164,10 +164,10 @@ const saucesArr = [{
     "originCountry": "united states of america",
     "peppers": "Red Ghost Pepper, Carolina Reaper",
     "scoville": "1000",
-    "updatedAt": "2023-02-11T14:19:54.193Z"
+    "updatedAt": "2023-01-11T14:19:54.193Z"
   },{
     "addedBy": "63e63f130c6136cbc2ff5e88",
-    "createdAt": "2023-03-25T03:51:27.000+00:00",
+    "createdAt": "2023-01-11T03:51:27.000+00:00",
     "description": "Hellfire's Fear This does a neat trick: being a superhot that delivers big flavor right before the pain sets in. With 60% of the bottle being a straight Carolina Reaper mash, once that pain arrives it's all you'll be focused on. But once the reaper wave subsides you'll be left to savory onion and garlic paired with bright citrus notes from lemon and lime. This sauce leaves you wanting more, in a scary way.",
     "image": "https://cdn.shopify.com/s/files/1/2086/9287/products/8190404984930.jpg?v=1628204768&width=680",
     "ingredients": "Reaper peppers, distilled vinegar, garlic, red wine vinegar, apple cider vinegar, sun dried tomatoes, onions, salt, tomatillos and citric acid, cilantro, curry, chili powder, black pepper, paprika, cumin, lime juice, lemon juice, coriander, ginger.",
@@ -177,10 +177,10 @@ const saucesArr = [{
     "originCountry": "united states of america",
     "peppers": "Reaper peppers",
     "scoville": "2000",
-    "updatedAt": "2023-02-11T14:19:54.193Z"
+    "updatedAt": "2023-01-11T14:19:54.193Z"
   },{
     "addedBy": "63e63f130c6136cbc2ff5e88",
-    "createdAt": "2023-03-25T03:51:27.000+00:00",
+    "createdAt": "2023-01-20T03:51:27.000+00:00",
     "description": "Looking to ruin your afternoon in the most delicious way possible? Then this is the sauce for you! With roughly 36 ghost peppers (bhut jolokia) in each bottle of sauce, the Dawson's XXX packs a wallop that sits with you for a while. But thankfully the flavor matches the heat bite for bite.As the heat slowly builds you have time to savor the pepper sweetness and rich, mouth coating flavor of ghost. Try it on a sandwich or a slice of pizza, or mixed into a stir fry or stew, or to add a much needed kick to some hummus.",
     "image": "https://cdn.shopify.com/s/files/1/2086/9287/products/1047102521353.jpg?v=1628202950&width=680",
     "ingredients": "Vinegar, dried ghost pepper, garlic, salt",
@@ -190,10 +190,10 @@ const saucesArr = [{
     "originCountry": "united states of america",
     "peppers": "ghost pepper",
     "scoville": "2500",
-    "updatedAt": "2023-02-11T14:19:54.193Z"
+    "updatedAt": "2023-01-11T14:19:54.193Z"
   },{
     "addedBy": "63e63f130c6136cbc2ff5e88",
-    "createdAt": "2023-03-25T03:51:27.000+00:00",
+    "createdAt": "2023-01-19T03:51:27.000+00:00",
     "description": "If we’ve learned anything from 11 seasons of Hot Ones, it’s that our friends down under like it HOT, and this Aussie-made sauce with its mix of 6 different super-spicy peppers is no exception. Sourced from local farmers in three different climate zones across Western Australia, the secret mixture of super-hots are used for a variation of flavors and heat profiles. Add cider vinegar, citrus and a hint of sea salt and you have a delicious concoction that hits like a KO combination.",
     "image": "https://cdn.shopify.com/s/files/1/2086/9287/products/widowmaker.jpg?v=1628203011&width=680",
     "ingredients": "Chili peppers, cider vinegar, lime, garlic, brown sugar, sea salt",
@@ -203,10 +203,10 @@ const saucesArr = [{
     "originCountry": "united states of america",
     "peppers": "Chili peppers",
     "scoville": "5000",
-    "updatedAt": "2023-02-11T14:19:54.193Z"
+    "updatedAt": "2023-01-11T14:19:54.193Z"
   },{
     "addedBy": "63e63f130c6136cbc2ff5e88",
-    "createdAt": "2023-03-25T03:51:27.000+00:00",
+    "createdAt": "2023-01-18T03:51:27.000+00:00",
     "description": "Dustin Poirier is bringing the heat! The UFC fan favorite has partnered with Heartbeat Hot Sauce to create Poirier’s Louisiana Style, a hot sauce ode to his Lafayette, Louisiana roots. Like any good cajun sauce, the recipe starts with the best cayenne peppers, fermented to round out the sting and up the flavor before finishing with vinegar, sea salt, celery and garlic. Add in a handful of red habaneros for an extra punch of heat, and you have a knock out sauce that can take on everything from heavyweight meals like biscuits and gravy to featherweight dishes like grilled chicken breast or cauliflower pizza. #PaidInFull #ElDiamante",
     "image": "https://cdn.shopify.com/s/files/1/2086/9287/products/poirier1.jpg?v=1628212700&width=680",
     "ingredients": "Vinegar, aged cayenne peppers, sea salt, red habanero, celery, garlic, canola oil",
@@ -216,10 +216,10 @@ const saucesArr = [{
     "originCountry": "united states of america",
     "peppers": "cayenne peppers",
     "scoville": "500",
-    "updatedAt": "2023-02-11T14:19:54.193Z"
+    "updatedAt": "2023-01-11T14:19:54.193Z"
   },{
     "addedBy": "63e63f130c6136cbc2ff5e88",
-    "createdAt": "2023-03-25T03:51:27.000+00:00",
+    "createdAt": "2023-01-17T03:51:27.000+00:00",
     "description": "He’s #PaidinFull and bringing the pain! The second hot sauce from UFC favorite Dustin Poirier, this bruiser combines the original Cajun flavor with ghost pepper heat for a knockout kick. Aged cayenne and mouth-puckering white vinegar give this sauce its iconic Louisiana hot sauce taste, while celery, sea salt and garlic add more interesting flavors. The perfect hot sauce for spicing up all your favorite Louisiana classics: jambalaya, gumbo, shrimp & grits and more! Also delicious on wings, eggs, pizza and all your other fight night snacks and protein packed meal preps!",
     "image": "https://cdn.shopify.com/s/files/1/2086/9287/products/PoirierKOhotsauce.jpg?v=1629742014&width=680",
     "ingredients": "Vinegar, aged cayenne peppers, sea salt, red habanero, celery, garlic, Smokin' Ed's Ghost Pepper Powder, canola oil",
@@ -229,10 +229,10 @@ const saucesArr = [{
     "originCountry": "united states of america",
     "peppers": "cayenne peppers, red habanero",
     "scoville": "2800",
-    "updatedAt": "2023-02-11T14:19:54.193Z"
+    "updatedAt": "2023-01-11T14:19:54.193Z"
   },{
     "addedBy": "63e63f130c6136cbc2ff5e88",
-    "createdAt": "2023-03-25T03:51:27.000+00:00",
+    "createdAt": "2023-01-16T03:51:27.000+00:00",
     "description": "Crafted in Chengdu, the heart of China’s Sichuan province, Sichuan Gold brings all the addicting umami notes of chili crisp to this hot sauce-meets-chili oil. Pungent aromatics like garlic and green onion actually take a backseat to the verdant floral flavor of the Sichuan peppercorn, which is enhanced by the nutty rapeseed oil. Sichuan Gold’s silky texture makes it ideal for marinating or for coating chicken wings. It can also be used as a finishing sauce for everything from dumplings to avocado toast to noodles. This hot sauce’s one-of-a-kind flavor is lighting up the fifth wing for Hot Ones Season 19!",
     "image": "https://cdn.shopify.com/s/files/1/2086/9287/products/Fly-By-Jing-Sichuan-Gold-Hot-Sauce-5-oz-1_2cb4af77-36bc-496f-864b-9037dffc382f.jpg?v=1663790675&width=680",
     "ingredients": "Non-GMO soybean oil, non-GMO sichuan rapeseed oil, chili peppers, sichuan peppers, ginger, garlic, rock sugar, star anise, cassia bark, green onions, chili oil extract",
@@ -242,10 +242,10 @@ const saucesArr = [{
     "originCountry": "united states of america",
     "peppers": "chili peppers, sichuan peppers",
     "scoville": "500",
-    "updatedAt": "2023-02-11T14:19:54.193Z"
+    "updatedAt": "2023-01-11T14:19:54.193Z"
   },{
     "addedBy": "63e63f130c6136cbc2ff5e88",
-    "createdAt": "2023-03-25T03:51:27.000+00:00",
+    "createdAt": "2023-01-15T03:51:27.000+00:00",
     "description": "Say “Aloha” to flavor with Hot Ones Season 18’s #2 sauce! A family business hailing from beautiful Hawaii, maker Adoboloco has infused this hot sauce with tropical flare thanks to fire-roasted pineapple. The caramelized pineapple takes on a whole new level of sweet goodness, then is blitzed with Cayenne peppers until it becomes silky smooth perfection. The perfect mild hot sauce for summer eats or when you want to be transported to an island paradise.",
     "image": "https://cdn.shopify.com/s/files/1/2086/9287/products/HOTONES-SEASON18Adoboloco.jpg?v=1652724983&width=680",
     "ingredients": "Pineapple, chili pepper, red wine vinegar, honey, onion powder, nutritional yeast, sea salt, mustard seed powder, spices",
@@ -255,10 +255,10 @@ const saucesArr = [{
     "originCountry": "united states of america",
     "peppers": "chili pepper",
     "scoville": "2000",
-    "updatedAt": "2023-02-11T14:19:54.193Z"
+    "updatedAt": "2023-01-11T14:19:54.193Z"
   },{
     "addedBy": "63e63f130c6136cbc2ff5e88",
-    "createdAt": "2023-03-25T03:51:27.000+00:00",
+    "createdAt": "2023-01-14T03:51:27.000+00:00",
     "description": "Keith Habersberger of The Try Guys is known for his love of fried chicken, especially buffalo wings and ranch. When he challenged HEATONIST to come up with a new hot sauce flavor concept this was a natural place to start. The resulting Keith’s Chicken Sauce channels iconic ranch flavors with dill, parsley, and chives, but sweetens them up with smoked onions, garlic scapes, and Vermont maple syrup. Vinegar, zippy serrano peppers and roasted red pepper add tang while sour cream powder adds a creamy texture that makes this a must-have condiment. Perfect on chicken of all kinds, this sauce cuts through rich foods like fries or a grilled cheese with ease. A must try on pizza!",
     "image": "https://cdn.shopify.com/s/files/1/2086/9287/products/Keith_shotsauce.jpg?v=1656427058&width=680",
     "ingredients": "Organic distilled vinegar, serrano peppers, maple wood smoked onions, pure Vermont maple syrup, garlic scapes, roasted red peppers ( red peppers, water, salt, citric acid) sunflower oil, sour cream powder ( sour cream powder cream cultures and lactic acid, cultured nonfat milk solids, citric acid) salt, dill, parsley, chives, black pepper",
@@ -268,10 +268,10 @@ const saucesArr = [{
     "originCountry": "united states of america",
     "peppers": "serrano peppers, roasted red peppers",
     "scoville": "60",
-    "updatedAt": "2023-02-11T14:19:54.193Z"
+    "updatedAt": "2023-01-11T14:19:54.193Z"
   },{
     "addedBy": "63e63f130c6136cbc2ff5e88",
-    "createdAt": "2023-03-25T03:51:27.000+00:00",
+    "createdAt": "2023-01-13T03:51:27.000+00:00",
     "description": "Fresh Jalapeño gives this sauce a distinctly Baja zing, amplified by the clear flavors of white tequila & plenty of lime juice. What takes this sauce to the next level is the sweet/tart flavor that comes from the green apple. Try it with seafood dishes, or to punch-up a margarita.",
     "image": "https://cdn.shopify.com/s/files/1/2086/9287/products/840325627913.jpg?v=1628213049&width=680",
     "ingredients": "White vinegar, jalapeño peppers, onions, lime juice, apple cider vinegar w/ mother, tequila, garlic, green apple, ginger root, olive oil, salt, and spices.",
@@ -281,10 +281,10 @@ const saucesArr = [{
     "originCountry": "united states of america",
     "peppers": "jalapeño peppers",
     "scoville": "200",
-    "updatedAt": "2023-02-11T14:19:54.193Z"
+    "updatedAt": "2023-01-14T14:19:54.193Z"
   },{
     "addedBy": "63e63f130c6136cbc2ff5e88",
-    "createdAt": "2023-03-25T03:51:27.000+00:00",
+    "createdAt": "2023-01-12T03:51:27.000+00:00",
     "description": "A new crowd favorite here at hot sauce HQ, the Humble House Ancho is a sauce that begs to be squeezed on everything. It's rich and smoky from the mild Morita pepper (a smoked red jalapeño) and slightly sweet from tamarind. This is a BBQ/hot sauce hybrid that works great on rice and beans and grilled meats. Or eggs. Or chicken. Or anything!",
     "image": "https://cdn.shopify.com/s/files/1/2086/9287/products/394375069705.jpg?v=1628209506&width=680",
     "ingredients": "Aged tamarind chili paste (raisins, tamarind concentrate, gluten-free soy sauce, balsamic vinegar, Morita chile, ancho chile, kosher salt), vinegar, cane sugar, fresh garlic",
@@ -294,10 +294,10 @@ const saucesArr = [{
     "originCountry": "united states of america",
     "peppers": "Morita chile, ancho chile",
     "scoville": "2000",
-    "updatedAt": "2023-02-11T14:19:54.193Z"
+    "updatedAt": "2023-01-13T14:19:54.193Z"
   },{
     "addedBy": "63e63f130c6136cbc2ff5e88",
-    "createdAt": "2023-03-25T03:51:27.000+00:00",
+    "createdAt": "2023-01-11T03:51:27.000+00:00",
     "description": "Cosmic Dumpling gets its name from sauce maker Gene’s former career as an engineer for NASA satellites! The hot sauce lives up to its out-of-this-world name with supernova-sized, sweet and savory flavors thanks to tamari, orange juice, honey, and garlic, all getting liftoff from a healthy dose of sesame oil. Like a black hole, it pulls in everything in its path - dumplings, spring rolls, bao and more. Also makes a universally good glaze, on pork belly, salmon, tofu or more!",
     "image": "https://cdn.shopify.com/s/files/1/2086/9287/products/cosmic-dumpling.jpg?v=1622751529&width=680",
     "ingredients": "Rice wine vinegar, red jalapeno peppers, tamari (water, soybeans, salt, alcohol), brown sugar, orange juice, garlic, tomato paste (fresh tomatoes, naturally sourced citric acid), lemon juice, honey, sesame oil, ginger, culantro, orange zest",
@@ -307,7 +307,7 @@ const saucesArr = [{
     "originCountry": "united states of america",
     "peppers": "red jalapeno peppers",
     "scoville": "1500",
-    "updatedAt": "2023-02-11T14:19:54.193Z"
+    "updatedAt": "2023-01-12T14:19:54.193Z"
   }]
 
 mongoose
