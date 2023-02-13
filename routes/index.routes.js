4b0fdf00b1a7cc0ed4d91113e8822589285e0ae7
Krystina Witt <krystinawitt@gmail.com>
2023-02-13 19:38:32 +0100

    middleware fixed for splash page

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard');
 const router = express.Router();
 
 /* GET home page */
-router.get("/", (req, res, next) => {
+router.get("/", isLoggedOut, (req, res, next) => {
   console.log(req.session)
   res.render("index", {user: req.session.user || undefined});
 });
